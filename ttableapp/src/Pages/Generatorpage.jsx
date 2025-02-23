09a5860c7aca59425b1b210c29c06aaa4faca275
darkky9197 <darkking9197@gmail.com>
2025-02-23 19:57:26 +0530

    form commit 2

--- ttableapp/src/Pages/Generatorpage.jsx
+++ ttableapp/src/Pages/Generatorpage.jsx
@@ -1,61 +1,80 @@
 import React, { useRef, useState } from 'react'
 
 function Generatorpage() {
-  const [radioVal,setRadioval] = useState("")
+  const [radioVal, setRadioval] = useState(false);
+  const [data, setData] = useState([]);
+
   const subname = useRef()
   const staffId = useRef()
   const count = useRef()
-  const values = useRef()
 
-  let datas=[]
-
-  //radio value access section
-  const handleChange = (val)=>{
-    setRadioval(val)
-  }
 
   //insert objects in an array section
-  const insertObject = ()=>{
-    datas.push({subName:subname.current.value,staff_id:staffId.current.value,isLab:radioVal,count:count.current.value})
-    console.log(datas)
+  function insertObject() {
+    let temp = [...data, { subName: subname.current.value, staff_id: staffId.current.value, isLab: radioVal, count: count.current.value }];
+    setData(temp);
   }
-  
+
   //design section
   const styles = {
-    form:{
+    form: {
       display: "flex",
       justifyContent: "center",
       alignItems: "center",
-      border:"1px solid black"
+      border: "1px solid black"
     },
-    input:{
-      marginTop:"10px"
+    input: {
+      marginTop: "10px"
     },
-    cont:{
-      border:"1px solid orange",
-      padding:"20px",
+    cont: {
+      border: "1px solid orange",
+      padding: "20px",
 
     }
   }
   return (
     <>
-        <form style={styles.form}>
-            <div style={styles.cont}>
-                <input type="text" ref={subname} name="subname" placeholder='Enter Subjet/Lab' style={styles.input}/><br />
-                <input type="text" ref={staffId} name="staffId" placeholder='Enter Staff ID' style={styles.input}/><br />
-                
-                <label style={styles.input} >Is lab</label><br />
-                <input type="radio" id="yes" value='Yes' checked={radioVal==="Yes"} onChange={()=>handleChange("Yes")}/>
-                <label htmlFor="Yes">Yes</label>
-        
-                <input type="radio" id="No" value="No" checked={radioVal==="No" } onChange={()=>handleChange("No")}/>
-                <label htmlFor="No">No</label><br />
+      <form style={styles.form}>
+        <div style={styles.cont}>
+          <input type="text" ref={subname} name="subname" placeholder='Enter Subjet/Lab' style={styles.input} /><br />
+          <input type="text" ref={staffId} name="staffId" placeholder='Enter Staff ID' style={styles.input} /><br />
 
-                <input type="number" ref={count} placeholder='Enter the subject count' name='count' style={styles.input} /><br />
-                <button type='reset' onClick={insertObject}>Add</button>
-            </div>
-        </form>
-        <h1 ref={values}></h1>
+          <label style={styles.input} >Is lab</label><br />
+          <input type="radio" id="yes" value={true} checked={radioVal === true} onChange={() => setRadioval(true)} />
+          <label htmlFor="Yes">Yes</label>
+
+          <input type="radio" id="No" value={false} checked={radioVal === false} onChange={() => setRadioval(false)} />
+          <label htmlFor="No">No</label><br />
+
+          <input type="number" ref={count} placeholder='Enter the subject count' name='count' style={styles.input} /><br />
+          <button type='reset' onClick={insertObject}>Add</button>
+        </div>
+      </form>
+      <div className='table-container'>
+        <table>
+          <thead>
+            <tr>
+              <td>Subject/Lab</td>
+              <td>Staff ID</td>
+              <td>isLab?</td>
+              <td>count</td>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((data, index) => {
+                <tr key={index}>
+                  <td>{data.subName}</td>
+                  <td>{data.staff_id}</td>
+                  <td>{data.isLab}</td>
+                  <td>{data.count}</td>
+                </tr>
+              })
+            }
+
+          </tbody>
+        </table>
+      </div>
     </>
   )
 }
